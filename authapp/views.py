6d770290f7859ec0fb9786bda331af79e762b6a3
Dev Gana <yahaya128173908@gmail.com>
2024-09-01 14:24:22 +0100

    document my view.py

--- authapp/views.py
+++ authapp/views.py
@@ -7,9 +7,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
 # Create your views here.
+
+#this function is to view the home index page
 def index(request):
     return render(request, 'auth/index.html')
 
+# this is the signup function which is responsible for viewing the signup page and also handle signing up fuctionalities
 def signup(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -49,6 +52,7 @@ def signup(request):
         return redirect('/login')
     return render(request, 'auth/signup.html')
 
+# this is the signin(login) function which is responsible for viewing the signin(login) page and also handle signing in or logging in fuctionalities
 def signin(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -65,14 +69,15 @@ def signin(request):
 
     return render(request, 'auth/login.html')
 
-
+# this is the signout(logout) function which is responsible for viewing the signout(logout) page and also handle signing out or login out fuctionalities
 def signout(request):
     logout(request)
     messages.success(request, "Logout successfull")
     return redirect('home')
 
-@login_required
-def dashboard(request):
+#this function is use to protect a function from guest. Only authenticated users can access it
+@login_required 
+def dashboard(request): # this is the dashboard function which is responsible for viewing the dashboard after authenticating
     return render(request, 'auth/dashboard.html')
 
 # class ProtectedView(LoginRequiredMixin, View):
